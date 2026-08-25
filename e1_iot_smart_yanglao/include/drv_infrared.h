fe12729c948e8cdc8f019abf3d1a4f8cac990766
luoyan0112 <w2832816054@163.com>
2026-08-25 07:36:30 +0000

    删除

--- e1_iot_smart_yanglao/include/drv_infrared.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DRV_INFRARED_H__
-#define __DRV_INFRARED_H__
-
-#include "stdbool.h"
-
-void infrared_dev_init(void);
-bool get_infrared_state(void); // true表示检测到人，false表示无人
-
-#endif
--- e1_iot_smart_yanglao/include/drv_sensors.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DRV_SENSORS_H__
-#define __DRV_SENSORS_H__
-
-
-void i2c_dev_init(void);
-void bh1750_read_data(double *dat);
-void sht30_read_data(double *temp, double *humi);
-
-#endif
